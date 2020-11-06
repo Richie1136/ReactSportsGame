aecb3c33db1068c931eebb1d5a7cf3f9c855219d
Richie1136 <Richiemajor1@gmail.com>
2020-11-06 09:42:36 -0500

    react sports game

---
+++ reactApp.js
@@ -0,0 +1,35 @@
+
+class Game extends React.Component {
+  state = {
+    venue: this.props.venue
+  }
+  
+    render() {
+      return (
+      <React.Fragment>
+        <h1 id = "venueName">Welcome to {this.props.venue}</h1>
+        <Team name={this.props.name} logo={this.props.logo} />
+        <div id="vsDiv">VS</div>
+        {/* <Team name={this.props.name} logo={this.props.logo} /> */}
+  
+      </React.Fragment>
+      )
+    }
+  }
+  
+  
+  function App(props){
+    return (
+      <React.Fragment>
+   <Game venue="Kobe Bryant Arena" />
+      <Team name="Lakers" logo ="./Kobe.png" className ="Lakers"  />
+      <Team name="BlueDevils" logo ="./duke basketball.jpg" className ="Duke" />
+    </React.Fragment>
+  )
+}
+
+//Render the application
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
---
+++ team.js
@@ -0,0 +1,59 @@
+const swishFile = "./Swish.wav"
+const backBoardFile = "./Back+Board.wav"
+
+const swishSound = new Audio(swishFile);
+const backBoardSound = new Audio(backBoardFile);
+
+class Team extends React.Component {
+state = {
+  name: this.props.name,
+  logo: this.props.logo,
+  shotsTaken: 0,
+  score: 0,
+  shotPercentage: 0,
+};
+
+
+score = () => {
+  this.setState((state) => ({score: state.score + 1 }));
+};
+
+
+
+addShots = () => {
+  this.setState((state) => ({ shotsTaken: state.shotsTaken + 1 }));
+let randomNumber = Math.floor(Math.random() * 4) + 1;
+if (randomNumber === 1) {
+  swishSound.play();
+  this.score();
+} else {
+  backBoardSound.play();
+}
+this.shotPercentage()
+}
+
+shotPercentage = () => {
+  this.setState((state) => { 
+      let percentage = (state.score/state.shotsTaken) * 100;
+    console.log(percentage)
+    return ({...state, shotPercentage: percentage});
+  
+  })
+}
+  render() {
+return (
+  <React.Fragment>
+  <div className = {this.props.className}>
+    {/* <h1>{this.props.name} </h1> */}
+    <img src = {this.props.logo} width= "300px" />
+    <p>Shots: {this.state.shotsTaken}</p>
+    <p>Score: {this.state.score}</p>
+{  this.state.shotsTaken > 0  &&  <p>shotPercentage: {this.state.shotPercentage}</p>
+}    <button onClick={this.addShots}>Shoot!</button>
+  </div>
+</React.Fragment>
+  );
+  }
+}
+
+
